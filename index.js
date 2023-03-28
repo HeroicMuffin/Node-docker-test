fff5c0c2af914c2b98ce252236c9378b027d879f
HeroicMuffin <theoneandonlypeleg@gmail.com>
2023-03-28 14:19:58 +0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 
 app.get("/",(req,res) => { 
-    res.send(`<h1>This is a test page for testing Docker container <br> still works</h1>`);
+    res.send(`<h1>This is a test page for testing Docker container <br> still works!!!</h1>`);
 })
 
 
